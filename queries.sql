b9c18ae92b52b2ff6fa198db301d32fd19af2546
AJ193 <119049613+AJ193@users.noreply.github.com>
2023-08-14 13:33:54 +0500

    initial commit

--- queries.sql
+++ queries.sql
@@ -1,10 +1,3 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT * from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT * from animals WHERE nuetered = TRUE AND escape_attempts < 3;
-SELECT  date_of_birth FROM animals WHERE  (  name = 'Agumon'  OR name = 'Pikachu'  );
-SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
- SELECT * from animals WHERE nuetered;
- SELECT * from animals WHERE name != 'Gabumon';
- SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT * from animals WHERE name = 'Luna';
